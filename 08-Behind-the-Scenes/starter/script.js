9b8a961d5dad465bc8d2c7de559eb9d4b798f277
John Gabriel Adawag <v-jgcadawag@meralco.com.ph>
2024-09-10 14:40:31 +0800

    finished strings section

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2024 - birthYear;
+    console.log(firstName);
+    return age;
+}
+
+const firstName = 'Gab';
+calcAge(1993);
+
+console.log(color);
+var color = 'red';
+
+console.log(myFunction);
+{
+    var myFunction = function () {
+        console.log('this is my function');
+    }
+}
